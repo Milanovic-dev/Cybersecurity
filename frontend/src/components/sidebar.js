7028ca5a44d5b272cdf89ef9e5bdcfa170250925
milanatucakov <milanatucakov@gmail.com>
2020-04-02 21:32:01 +0200

    Initial frontend commit

---
+++ frontend/src/components/sidebar.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import Isvg from 'react-inlinesvg';
+import { Link } from 'react-router-dom';
+import menu from '../assets/svg/menu.svg';
+
+import list from '../assets/svg/list.svg';
+import add from '../assets/svg/add.svg';
+import rocket from '../assets/svg/rocket.svg';
+import mail from '../assets/svg/mail.svg';
+import settings from '../assets/svg/settings.svg';
+import exit from '../assets/svg/exit.svg';
+import home from '../assets/svg/home.svg';
+
+
+
+
+class Sidebar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            _show: true
+        };
+    }
+
+
+
+
+    render() {
+
+        return (
+
+            <div className={this.state._show ? `sidebar` : 'sidebar sidebar-hidden'}>
+                <div className="items">
+                    <h6>ITEMS</h6>
+                    <ul>
+
+                        <li>
+                            <Link to='/admin/list' className={this.props[0].location.pathname == '/admin/list' ? 'active' : null}>
+                                <Isvg src={list} />
+                                All items
+                            </Link>
+                        </li>
+
+                        <li>
+                            <Link to='/admin/list/new' className={this.props[0].location.pathname == '/admin/list/new' ? 'active' : null}>
+                                <Isvg src={add} />
+                                Create
+                            </Link>
+                        </li>
+                    </ul>
+
+                    <ul className="logout">
+                        <li id="logout" onClick={() => { localStorage.removeItem('token'); localStorage.removeItem('clinicAdminToken'); localStorage.removeItem('clinicUserToken'); localStorage.removeItem('patientToken'); }}>
+                            <Link to='/login' id="logout-link" >
+                                <Isvg src={exit} />
+                                Odjavi se
+                            </Link>
+                        </li>
+                    </ul>
+
+
+                </div>
+
+
+                <div className="menu" onClick={() => this.setState({ _show: !this.state._show })}>
+                    <Isvg src={menu} />
+                </div>
+
+            </div >
+        )
+    }
+
+};
+
+export default Sidebar;
---
+++ frontend/src/routes.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
+
+import AdminLoginPage from './views/admin/login';
+import ExampleListPage from './views/admin/exampleListPage';
+import ExampleFormPage from './views/admin/exampleFormPage';
+
+class Routes extends Component {
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <Router >
+                <div>
+                    <Switch className="react-switch">
+                        <Route
+                            path="/"
+                            exact
+                            render={(...renderProps) => (
+                                <AdminLoginPage {...renderProps} {...this.props} />
+                            )}
+                        />
+
+                        <Route
+                            path="/login"
+                            exact
+                            render={(...renderProps) => (
+                                <AdminLoginPage {...renderProps} {...this.props} />
+                            )}
+                        />
+
+                
+
+                        <Route
+                            path="/admin/list"
+                            exact
+                            render={(...renderProps) => (
+                                <ExampleListPage {...renderProps} {...this.props} />
+                            )}
+                        />
+
+                        <Route
+                            path="/admin/list/:id"
+                            exact
+                            render={(...renderProps) => (
+                                <ExampleFormPage {...renderProps} {...this.props} />
+                            )}
+                        />
+
+
+                    </Switch>
+                </div>
+            </Router >
+        );
+    }
+}
+
+export default Routes;
