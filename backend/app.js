20b2cca07c02edb5e08b136c38c7e282f2afae67
Milan Stanojevic <milan@novamedia.agency>
2020-04-08 02:06:13 +0200

    Creating certificates chain and fixed parsing

--- backend/app.js
+++ backend/app.js
@@ -11,11 +11,19 @@ const isAdminAuthenticated = require('./admin/auth');
 
 const exampleModule = new (require('./example/example'))();
 
+const logger = function (req, res, next) {
+
+
+
+    console.log(req.headers);
+    next(); // Passing the request to the next handler in the stack.
+}
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use('/uploads', express.static('uploads'))
+app.use(logger);
 const server = http.createServer(app);
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
@@ -58,26 +66,31 @@ app.get('/example/query', async (req, res) => {
 
 
 async function test(){
-    let result = await generateCertificate({
+    let rootResult = await generateCertificate({
         serialNumber: 1,
         issuer: {
             country: 'BA',
-            organizationName: 'Test',
+            organizationName: 'CybersecurityRoot',
             organizationalUnit: 'Test',
-            commonName: 'clickr.app',
+            commonName: 'CybersecurityRoot',
             localityName: 'Bijeljina',
-            stateName: 'BA',
+            stateName: 'RS',
             email: 'stanojevic.milan97@gmail.com'
         },
         subject: {
-            commonName: 'clickr.app',
+            country: 'BA',
+            organizationName: 'CybersecurityRoot',
+            organizationalUnit: 'Test',
+            commonName: 'CybersecurityRoot',
+            localityName: 'Bijeljina',
+            stateName: 'RS',
+            email: 'stanojevic.milan97@gmail.com'
         },
-
         validFrom: new Date(2020, 1, 1),
         validTo: new Date(2021, 1, 1),
         basicConstraints: {
             isCA: true,
-            pathLengthConstraint: 3
+            pathLengthConstraint: 2
         },
         extendedKeyUsage: [
             "anyExtendedKeyUsage",
@@ -90,11 +103,85 @@ async function test(){
             "MicrosoftCertificateTrustListSigning",
             "MicrosoftEncryptedFileSystem"
         ]    
-    });
+    }, null);
+
+
+    fs.writeFileSync('root.crt', rootResult.certificate);
+    fs.writeFileSync('root.key', rootResult.privateKey);
+
+    let intermediateResult = await generateCertificate({
+        serialNumber: 2,
+        subject: {
+            country: 'BA',
+            organizationName: 'CybersecurityIntermediate',
+            organizationalUnit: 'Test',
+            commonName: 'CybersecurityIntermediate',
+            localityName: 'Bijeljina',
+            stateName: 'RS',
+            email: 'stanojevic.milan97@gmail.com'
+        },
+        validFrom: new Date(2020, 1, 1),
+        validTo: new Date(2021, 1, 1),
+        basicConstraints: {
+            isCA: true,
+            pathLengthConstraint: 1
+        },
+        extendedKeyUsage: [
+            "anyExtendedKeyUsage",
+            "serverAuth",
+            "clientAuth",
+            "codeSigning",
+            "emailProtection",
+            "timeStamping",
+            "OCSPSigning",
+            "MicrosoftCertificateTrustListSigning",
+            "MicrosoftEncryptedFileSystem"
+        ]    
+    }, rootResult.certificate, rootResult.privateKey );
+
+
+    fs.writeFileSync('intermediate.crt', intermediateResult.certificate);
+    fs.writeFileSync('intermediate.key', intermediateResult.privateKey);
+
+
+    let endEntityResult = await generateCertificate({
+        serialNumber: 3,
+        subject: {
+            country: 'BA',
+            organizationName: 'CybersecurityEndEntity',
+            organizationalUnit: 'Test',
+            commonName: 'localhost',
+            localityName: 'Bijeljina',
+            stateName: 'RS',
+            email: 'stanojevic.milan97@gmail.com'
+        },
+        validFrom: new Date(2020, 1, 1),
+        validTo: new Date(2021, 1, 1),
+        basicConstraints: {
+            isCA: false,
+            pathLengthConstraint: null
+        },
+        extendedKeyUsage: [
+            "anyExtendedKeyUsage",
+            "serverAuth",
+            "clientAuth",
+            "codeSigning",
+            "emailProtection",
+            "timeStamping",
+            "OCSPSigning",
+            "MicrosoftCertificateTrustListSigning",
+            "MicrosoftEncryptedFileSystem"
+        ]    
+    }, intermediateResult.certificate, intermediateResult.privateKey );
+
+
+    fs.writeFileSync('endEntity.crt', endEntityResult.certificate);
+    fs.writeFileSync('endEntity.key', endEntityResult.privateKey);
+
 
     //console.log(result.certificate);
-    //console.log(parseCertificate(fs.readFileSync('google.crt', 'utf8')))
-    //fs.writeFileSync('test.cer', result.certificate);
+    //console.log(JSON.stringify( await parseCertificate(fs.readFileSync('test.cer', 'utf8')), null ,4) )
+    
     //console.log(parseCertificate(result.certificate));
 }
 
